639c08b2d8ad46052acf830c00a0e01e00ea8443
Tobias Stahn <Tobias.Stahn@mehrwert.de>
2023-09-06 10:22:18 +0200

    [TASK] Re-work backend module

--- ext_tables.php
+++ ext_tables.php
@@ -1,31 +1,25 @@
 <?php
+
 defined('TYPO3_MODE') || die();
 
-call_user_func(
-    function () {
-        $extKey = 'fal_quota';
+(static function() {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'Mehrwert.FalQuota',
+        'file',
+        'falquota',
+        'bottom',
+        [
+            \Mehrwert\FalQuota\Controller\DashboardController::class => 'index',
+        ],
+        [
+            'access' => 'user,group',
+            'icon' => 'EXT:fal_quota/Resources/Public/Images/Icons/module-icon.svg',
+            'labels' => 'LLL:EXT:fal_quota/Resources/Private/Language/locallang_mod.xlf',
+            'navigationComponentId' => '',
+        ]
+    );
 
-        if (TYPO3_MODE === 'BE') {
-
-            // Add submodule `falquota`
-            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                'Mehrwert.FalQuota',
-                'file',
-                'falquota',
-                'bottom',
-                [
-                    \Mehrwert\FalQuota\Controller\DashboardController::class => 'index',
-                ],
-                [
-                    'access' => 'user,group',
-                    'icon' => 'EXT:' . $extKey . '/Resources/Public/Images/Icons/module-icon.svg',
-                    'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_mod.xlf',
-                    'navigationComponentId' => '',
-                ]
-            );
-            // Register DatamapDataHandlerHook
-            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['fal_quota'] =
-                \Mehrwert\FalQuota\Hooks\DatamapDataHandlerHook::class;
-        }
-    }
-);
+    // Register DatamapDataHandlerHook
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['fal_quota'] =
+        \Mehrwert\FalQuota\Hooks\DatamapDataHandlerHook::class;
+})();
