12e43d4bf68ec6197d42086322e6816edfcadb88
Tobias Stahn <Tobias.Stahn@mehrwert.de>
2024-05-29 10:55:31 +0200

    [BUGFIX] Remove undefined variable $_EXTKEY from ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,12 @@
 <?php
+
 defined('TYPO3_MODE') or die();
 
-/*
- * 2019 - EXT:fal_quota -FAL Quota
- *
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.md', which is part of this source code package.
- */
+(static function() {
+    // Register the class to be available in 'eval' of TCA
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\Mehrwert\FalQuota\Evaluation\StorageQuotaEvaluation::class] = '';
 
-call_user_func(
-    function ($extKey) {
-        // Register the class to be available in 'eval' of TCA
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\Mehrwert\FalQuota\Evaluation\StorageQuotaEvaluation::class] = '';
-
-        // Register UpdateWizard
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['falQuota_commandIdentifierUpdate']
-            = \Mehrwert\FalQuota\Updates\CommandIdentifierUpdateWizard::class;
-    },
-    $_EXTKEY
-);
+    // Register UpdateWizard
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['falQuota_commandIdentifierUpdate']
+        = \Mehrwert\FalQuota\Updates\CommandIdentifierUpdateWizard::class;
+})();
