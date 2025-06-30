be079b897727287e75e359a8c2724932e447f0fa
Olaf Schmidt-Wischhöfer <olaf.schmidt-wischhoefer@mehrwert.de>
2025-06-30 12:22:27 +0200

    [CHANGE] Avoid misleading error messages in backend
    * In case of problems when copying, moving or creating files or directories,
      TYPO3 shows "There might be a problem with write permissions".
      This is misleading if the error is caused by quota.

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,5 +4,11 @@ use Mehrwert\FalQuota\Evaluation\StorageQuotaEvaluation;
 
 defined('TYPO3') or die();
 
+// Remove "There might be a problem with write permissions" from backend file error messages.
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:core/Resources/Private/Language/fileMessages.xlf'][]
+    = 'EXT:fal_quota/Resources/Private/Language/fileMessages.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:core/Resources/Private/Language/fileMessages.xlf'][]
+    = 'EXT:fal_quota/Resources/Private/Language/de.fileMessages.xlf';
+
 // Register the class to be available in 'eval' of TCA
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][StorageQuotaEvaluation::class] = '';
